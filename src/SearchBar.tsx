111d8f97c6937d9eb0880a9a6e56b0bb1d6f938a
Adam <adam_mikulas@mckinsey.com>
2024-01-17 16:02:43 +0100

    feat: add searchbar functionality

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,7 +1,30 @@
+import { FormEvent, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
 function SearchBar() {
+  const [error, setError] = useState<string>();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    const { query: queryValue } = event.target as typeof event.target & {
+      query: { value: string };
+    };
+    if (queryValue?.value?.length < 3 || !queryValue?.value) {
+      setError('Please provide a search query that at least 2 characters long');
+      return;
+    }
+    searchParams.set('query', queryValue.value);
+    setSearchParams(searchParams);
+  };
+
   return (
-    <form className="flex gap-2">
-      <input type="text" className="border border-solid" />
+    <form className="flex gap-2" onSubmit={onSubmit}>
+      <div className="flex flex-col">
+        <label>Search for movies:</label>
+        <input type="text" className="border border-solid" name="query" />
+        {error && <p>{error}</p>}
+      </div>
       <button>Search</button>
     </form>
   );
