369984dcefeaece035c63cb3102d0870fec232d0
Adam <adam_mikulas@mckinsey.com>
2024-01-17 15:23:38 +0100

    feat: add searchbar skeleton

---
+++ src/SearchBar.tsx
@@ -0,0 +1,10 @@
+function SearchBar() {
+  return (
+    <form className="flex gap-2">
+      <input type="text" className="border border-solid" />
+      <button>Search</button>
+    </form>
+  );
+}
+
+export default SearchBar;
